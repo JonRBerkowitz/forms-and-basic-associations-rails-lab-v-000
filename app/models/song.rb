5ff74e8622989afc26bedbff087f621d5c203d63
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-19 18:19:13 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,18 +4,32 @@ class Song < ActiveRecord::Base
   has_many :notes
 
   def genre_name=(name)
-    self.genre = Genre.find_or_create_by(name: name)
+    genre = Genre.find_or_create_by(name: name)
+    self.genre = genre
   end
 
   def genre_name
-    self.genre.name
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by(name: name)
+    self.try(:genre).try(:name)
   end
 
   def artist_name
-    self.artist.name
+    self.try(:artist).try(:name)
+  end
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist = artist
+  end
+
+  def note_contents=(notes)
+    notes.each do |content|
+      if content.strip != ''
+        self.notes.build(content: content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map(&:content)
   end
 end
