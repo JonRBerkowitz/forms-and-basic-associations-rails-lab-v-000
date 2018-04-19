fd9cb8659506d4aebd743744f11a03da254af0c5
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2018-04-19 18:11:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -50,7 +50,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
-def song_params
-    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
-  end
